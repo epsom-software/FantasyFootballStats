3dd330d07cfde5110f698f3bb19cfe0648166c65
buhbuh <JuggleDrum@hotmail.co.uk>
2013-06-29 20:38:16 +0100

    Clicking the legend now toggles all checkboxen.

--- www/Scripts/Inputs.js
+++ www/Scripts/Inputs.js
@@ -1,18 +1,24 @@
 ///<reference path='Definitions\jquery.d.ts'/>
 $(document).ready(function () {
     var $selectors = $("input[type=radio], input[type=checkbox]");
-    function setupInputs() {
+    //See if the radios and checkboxen are checked, and then toggle the cssClass accordingly.
+    //Do this for all the inputs on the page, so that we can be sure they are all in sync.
+    function syncLabelStyleToInputValue() {
         $selectors.each(function () {
-            var $selector = $(this);
-            var $label = $("label[for=" + $selector.attr("id") + "]");
-            if(this.checked) {
-                $label.addClass("on");
-            } else {
-                $label.removeClass("on");
-            }
+            var id = $(this).attr("id");
+            var $label = $("label[for=" + id + "]");
+            $label.toggleClass("on", this.checked);
         });
     }
-    $selectors.change(setupInputs);
-    setupInputs();
+    $selectors.change(syncLabelStyleToInputValue);
+    syncLabelStyleToInputValue();
+    ///When a legend is clicked, turn on it's checkboxen, unless they were all already on.
+    $("legend").click(function () {
+        var $checkboxen = $(this).siblings("input[type=checkbox]");
+        var numberNotChecked = $checkboxen.not(":checked").length;
+        var turnOn = numberNotChecked > 0;
+        $checkboxen.prop('checked', turnOn);
+        syncLabelStyleToInputValue();
+    });
 });
 //@ sourceMappingURL=Inputs.js.map
