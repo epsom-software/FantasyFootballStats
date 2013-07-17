c6a4601cda2ecf419d73962a65c92b4df4b317bd
buhbuh <JuggleDrum@hotmail.co.uk>
2013-07-17 21:37:50 +0100

    Node server returns json

--- www/Node/server.js
+++ www/Node/server.js
@@ -3,11 +3,22 @@
 var server;
 (function (server) {
     var http = require('http');
+    var url = require('url');
     var port = process.env.PORT || 1337;
+    var QueryEngine = require("./queryEngine.js");
+
+    QueryEngine.Runner.players = require("./tests/playerRepository.js").players;
 
     http.createServer(function (req, res) {
+        var url_parts = url.parse(req.url, true);
+        var code = url_parts.query.code;
+
+        var queryEngine = new QueryEngine.Runner(code);
+        var result = queryEngine.run();
+
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello World from Node.js and TypeScript\n');
+
+        res.end(JSON.stringify(result));
     }).listen(port);
 })(server || (server = {}));
 //@ sourceMappingURL=server.js.map
--- www/Node/server.ts
+++ www/Node/server.ts
@@ -5,12 +5,23 @@
 module server {
 
     var http = require('http');
+    var url = require('url');
     var port = process.env.PORT || 1337;
+    var QueryEngine = require("./queryEngine.js");
+    
+    QueryEngine.Runner.players = require("./tests/playerRepository.js").players;
 
     http.createServer(function (req, res) {
+        
+        var url_parts = url.parse(req.url, true);
+        var code = url_parts.query.code;
+        
+        var queryEngine: QueryEngine.Runner = new QueryEngine.Runner(code);
+        var result = queryEngine.run();
 
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello World from Node.js and TypeScript\n');
+
+        res.end(JSON.stringify(result));
 
     }).listen(port);
 } 
