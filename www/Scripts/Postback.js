f9733bb798f800be264f73a97be0c5e081567e0e
buhbuh <JuggleDrum@hotmail.co.uk>
2013-07-18 22:17:27 +0100

    Refactored Postback.ts to be more TypeScripty

--- www/Scripts/Postback.js
+++ www/Scripts/Postback.js
@@ -1,42 +1,56 @@
 ///<reference path='Definitions\jquery.d.ts'/>
-$(document).ready(function () {
-    function toReadableEnglish(value) {
-        return value.split("_").map(function (word) {
-            return word.charAt(0).toUpperCase() + word.slice(1);
-        }).join(" ");
-    }
+var Postback;
+(function (Postback) {
+    var Format = (function () {
+        function Format() {
+        }
+        Format.toReadableEnglish = function (value) {
+            return value.split("_").map(function (word) {
+                return word.charAt(0).toUpperCase() + word.slice(1);
+            }).join(" ");
+        };
 
-    function toHtmlRow(values, dataElementName) {
-        var tableDatas = values.join("</" + dataElementName + "><" + dataElementName + ">");
-        var row = "<tr><" + dataElementName + ">" + tableDatas + "</" + dataElementName + "></tr>";
-        return row;
-    }
+        Format.toHtmlRow = function (values, dataElementName) {
+            var tableDatas = values.join("</" + dataElementName + "><" + dataElementName + ">");
+            var row = "<tr><" + dataElementName + ">" + tableDatas + "</" + dataElementName + "></tr>";
+            return row;
+        };
 
-    function callback(json) {
-        if (json && json.length > 0) {
-            var keys = Object.keys(json[0]);
+        Format.toHtmlTable = function (values) {
+            var keys = Object.keys(values[0]);
 
-            var headingRow = toHtmlRow(keys.map(toReadableEnglish), "th");
+            var headingRow = Format.toHtmlRow(keys.map(Format.toReadableEnglish), "th");
 
-            var rows = json.map(function (player) {
+            var rows = values.map(function (player) {
                 var values = keys.map(function (k) {
                     return player[k];
                 });
-                var row = toHtmlRow(values, "td");
+                var row = Format.toHtmlRow(values, "td");
                 return row;
             });
 
             var table = "<table>" + headingRow + rows.join("") + "</table>";
+            return table;
+        };
+        return Format;
+    })();
 
+    function callback(json) {
+        if (json && json.length > 0) {
+            var table = Format.toHtmlTable(json);
             $("#result").html(table);
         }
     }
 
-    $("form#statsForm").submit(function () {
-        $.getJSON("Node/server.js", { code: $("#code").val() }, callback);
-        return false;
-    });
+    function init() {
+        $("form#statsForm").submit(function () {
+            $.getJSON("Node/server.js", { code: $("#code").val() }, callback);
+            return false;
+        });
 
-    $("input").last().click();
-});
+        $("input").last().click();
+    }
+
+    $(document).ready(init);
+})(Postback || (Postback = {}));
 //@ sourceMappingURL=Postback.js.map
--- www/Scripts/Postback.ts
+++ www/Scripts/Postback.ts
@@ -1,45 +1,56 @@
 ///<reference path='Definitions\jquery.d.ts'/>
 
-$(document).ready(function () {
-    
-    function toReadableEnglish(value: string) {
-        return value.split("_").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
-    }
+module Postback {
 
-    function toHtmlRow(values: string[], dataElementName: string) {
-        var tableDatas: string = values.join("</" + dataElementName + "><" + dataElementName + ">");
-        var row: string = "<tr><" + dataElementName + ">" + tableDatas + "</" + dataElementName + "></tr>";
-        return row;
-    }
-    
-    function callback(json: Array) {
+    class Format {
 
-        if (json && json.length > 0) {
+        static toReadableEnglish(value: string): string {
+            return value.split("_").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+        }
 
-            var keys: Array = Object.keys(json[0]);
+        static toHtmlRow(values: string[], dataElementName: string): string {
+            var tableDatas: string = values.join("</" + dataElementName + "><" + dataElementName + ">");
+            var row: string = "<tr><" + dataElementName + ">" + tableDatas + "</" + dataElementName + "></tr>";
+            return row;
+        }
 
-            var headingRow = toHtmlRow(keys.map(toReadableEnglish), "th");
+        public static toHtmlTable(values: Array): string {
 
-            var rows: string[] = json.map(player => {
+            var keys: Array = Object.keys(values[0]);
+
+            var headingRow = Format.toHtmlRow(keys.map(Format.toReadableEnglish), "th");
+
+            var rows: string[] = values.map(player => {
                 var values: Array = keys.map(k => player[k]);
-                var row = toHtmlRow(values, "td");
+                var row = Format.toHtmlRow(values, "td");
                 return row
             })
 
             var table: string = "<table>" + headingRow + rows.join("") + "</table>";
+            return table;
+        }
+    }
 
+    function callback(json: Array) {
+
+        if (json && json.length > 0) {
+            var table: string = Format.toHtmlTable(json);
             $("#result").html(table);
         }
     }
 
-    $("form#statsForm").submit(function () {
-        $.getJSON(
-            "Node/server.js",
-            { code: $("#code").val() },
-            callback
-            );
-        return false;
-    });
+    function init() {
+        $("form#statsForm").submit(function () {
+            $.getJSON(
+                "Node/server.js",
+                { code: $("#code").val() },
+                callback
+                );
+            return false;
+        });
 
-    $("input").last().click();
-});
+        $("input").last().click();
+    }
+
+    $(document).ready(init);
+}
