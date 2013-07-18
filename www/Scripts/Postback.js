838f1b0c71fcbfa008fc8e36114bd76a9fb2ab67
buhbuh <JuggleDrum@hotmail.co.uk>
2013-07-18 23:23:08 +0100

    Team filter

--- www/Scripts/Postback.js
+++ www/Scripts/Postback.js
@@ -39,17 +39,95 @@ var Postback;
         if (json && json.length > 0) {
             var table = Format.toHtmlTable(json);
             $("#result").html(table);
+        } else {
+            $("#result").html("No results");
         }
     }
 
     function init() {
+        $(".Fields label").each(function () {
+            var field = $(this).text();
+            field = Format.toReadableEnglish(field);
+            $(this).text(field);
+        });
+
         $("form#statsForm").submit(function () {
             $.getJSON("Node/server.js", { code: $("#code").val() }, callback);
             return false;
         });
 
-        $("input").last().click();
+        $("input").last().click().click(QueryBuilder.build);
     }
+
+    var QueryBuilder = (function () {
+        function QueryBuilder() {
+        }
+        QueryBuilder.build = function () {
+            var query = QueryBuilder.select() + QueryBuilder.where();
+            $("#code").val(query);
+            return query;
+        };
+
+        QueryBuilder.select = function () {
+            var select = "";
+
+            var v = $(".Fields label.on").each(function () {
+                select += $(this).data("field") + " ";
+            });
+
+            if (select == "") {
+                select = " select * ";
+            } else {
+                select = " select " + select;
+            }
+
+            return select;
+        };
+
+        QueryBuilder.where = function () {
+            var where = "";
+
+            var maxCost = (function () {
+                var maxCost = $("input[name=maxCost]").val();
+                if (maxCost) {
+                    return parseFloat(maxCost);
+                } else {
+                    return 0;
+                }
+            })();
+
+            if (maxCost > 0) {
+                //For some reason the backend values are 10 times higher than the display values.
+                //Need to think about how we want to handle this.
+                maxCost *= 10;
+                where += "(now_cost <= " + maxCost + ") ";
+            }
+
+            var teams = [];
+
+            $(".Teams label.on").each(function () {
+                teams.push($(this).text());
+            });
+
+            if (teams.length > 0 && teams.length < 20) {
+                if (where != "") {
+                    where += " and ";
+                }
+
+                var teamFilter = teams.map(function (t) {
+                    return " (team_name = '" + t + "') ";
+                }).join(" or ");
+                where += " ( " + teamFilter + " ) ";
+            }
+
+            if (where != "") {
+                where = " where " + where;
+            }
+
+            return where;
+        };
+        return QueryBuilder;
+    })();
 
     $(document).ready(init);
 })(Postback || (Postback = {}));
--- www/Scripts/Postback.ts
+++ www/Scripts/Postback.ts
@@ -4,11 +4,11 @@ module Postback {
 
     class Format {
 
-        static toReadableEnglish(value: string): string {
+        public static toReadableEnglish(value: string): string {
             return value.split("_").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
         }
 
-        static toHtmlRow(values: string[], dataElementName: string): string {
+        private static toHtmlRow(values: string[], dataElementName: string): string {
             var tableDatas: string = values.join("</" + dataElementName + "><" + dataElementName + ">");
             var row: string = "<tr><" + dataElementName + ">" + tableDatas + "</" + dataElementName + "></tr>";
             return row;
@@ -36,10 +36,19 @@ module Postback {
         if (json && json.length > 0) {
             var table: string = Format.toHtmlTable(json);
             $("#result").html(table);
+        } else {
+            $("#result").html("No results");
         }
     }
 
     function init() {
+
+        $(".Fields label").each(function () {
+            var field = $(this).text();
+            field = Format.toReadableEnglish(field);
+            $(this).text(field);
+        });
+
         $("form#statsForm").submit(function () {
             $.getJSON(
                 "Node/server.js",
@@ -49,8 +58,75 @@ module Postback {
             return false;
         });
 
-        $("input").last().click();
+        $("input").last().click().click(QueryBuilder.build);
     }
+
+    class QueryBuilder {
+        public static build(): string {
+            var query = QueryBuilder.select() + QueryBuilder.where();
+            $("#code").val(query);
+            return query;
+        }
+
+        private static select(): string {
+            var select = "";
+
+            var v = $(".Fields label.on").each(function () {
+                select += $(this).data("field") + " ";
+            });
+
+            if (select == "") {
+                select = " select * ";
+            } else {
+                select = " select " + select;
+            }
+
+            return select;
+        }
+
+        private static where(): string {
+            
+            var where = "";
+
+            var maxCost: number = (function () {
+                var maxCost: any = $("input[name=maxCost]").val();
+                if (maxCost) {
+                    return parseFloat(maxCost);
+                } else {
+                    return 0;
+                }
+            })();
+
+            if (maxCost > 0) {
+                //For some reason the backend values are 10 times higher than the display values.
+                //Need to think about how we want to handle this.
+                maxCost *= 10;
+                where += "(now_cost <= " + maxCost + ") ";
+            }
+
+            var teams: string[] = [];
+            
+            $(".Teams label.on").each(function () {
+                teams.push($(this).text());
+            });
+
+            if (teams.length > 0 && teams.length < 20) {
+                if (where != "") {
+                    where += " and ";
+                }
+
+                var teamFilter = teams.map(t => " (team_name = '" + t + "') ").join(" or ");
+                where += " ( " + teamFilter + " ) ";
+            }
+
+            if (where != "") {
+                where = " where " + where;
+            }
+
+            return where;
+        }
+    }
+
 
     $(document).ready(init);
 }
