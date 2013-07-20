9b733775975dafda679fa31f19619687e9ccdbd6
buhbuh <JuggleDrum@hotmail.co.uk>
2013-07-20 21:48:00 +0100

    Hidden the code window in release

---
+++ www/Models/DebugHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FantasyFootballStats.Models
+{
+    public static class DebugHelper
+    {
+        public static bool IsDebug
+        {
+            get
+            {
+#if DEBUG
+                return true;
+#else
+                return false;
+#endif
+                }
+            }
+    }
+}
