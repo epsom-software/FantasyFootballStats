4b99e2812d0b0b66dc0241c6060e9d2be955a465
buhbuh <JuggleDrum@hotmail.co.uk>
2013-07-20 16:49:47 +0100

    Edit the downloaded json.

--- utils/DownloadPlayer.js
+++ utils/DownloadPlayer.js
@@ -7,13 +7,61 @@
 //npm install fs
 //node ./utils/DownloadPlayer 13
 
+function renameProperty(object, oldName, newName) {
+    var temp = object[oldName];
+    delete object[oldName];
+    object[newName] = temp;
+}
+function scaleCost(object, property) {
+    object[property] = object[property] / 10;
+}
+
+
 var request = require('request');
 var fs = require('fs');
 
 var playerId = process.argv[2];
 
 var url = 'http://fantasy.premierleague.com/web/api/elements/' + playerId + '/';
-var file = './www/Node/Data/Player/' + playerId + '.json';
+var file = './utils/Player/' + playerId + '.json';
 
-request(url).pipe(fs.createWriteStream(file))
+var json = "";
+request(url).on("data", function(chunk) {
+    chunk = chunk.toString();
+    chunk = chunk.replace(/_/g, "");
+    json += chunk;
+}).on("end", function() {
 
+    json = JSON.parse(json);
+    
+    //Delete attributes we dont care about:
+    delete json.photomobileurl;
+    delete json.shirtimageurl;
+    delete json.shirtmobileimageurl;
+    delete json.newsupdated;
+    delete json.newsadded;
+    delete json.elementtypeid;
+    delete json.teamcode;
+    
+    renameProperty(json, "webname", "name");
+    renameProperty(json, "nowcost", "cost");
+    
+    
+    //Move the larger properies to the bottom of the output:
+    renameProperty(json, "seasonhistory", "seasonhistory");
+    renameProperty(json, "fixtures", "fixtures");
+    
+    scaleCost(json, "eventcost")
+    scaleCost(json, "maxcost")
+    scaleCost(json, "mincost")
+    scaleCost(json, "cost")
+    scaleCost(json, "originalcost")
+    
+    json = JSON.stringify(json, undefined, 2);
+    json = json.replace('"seasonhistory"', '\r\n\r\n"seasonhistory"');
+    json = json.replace('"fixtures"', '\r\n\r\n"fixtures"');
+    
+    console.log(json);
+    
+    fs.writeFile(file, json);
+});
