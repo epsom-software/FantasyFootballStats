8ead70d005d29d7e3c9cb79d6a91d71cb636b96f
buhbuh <JuggleDrum@hotmail.co.uk>
2013-07-21 14:03:04 +0100

    Remove unnecessary checkboxes, because we are only using Ajax postbacks.

--- www/Scripts/Inputs.ts
+++ www/Scripts/Inputs.ts
@@ -3,28 +3,27 @@
 (function ($) {
     $(document).ready(function () {
 
-        var $selectors = $("input[type=radio], input[type=checkbox]");
+        $(".boxen label").click(function () {
+            
+            $(this).toggleClass("on");
 
-        //See if the radios and checkboxen are checked, and then toggle the cssClass accordingly.
-        //Do this for all the inputs on the page, so that we can be sure they are all in sync.
-        function syncLabelStyleToInputValue() {
-            $selectors.each(function () {
-                var id = $(this).attr("id");
-                var $label = $("label[for=" + id + "]");
-                $label.toggleClass("on", this.checked);
-            });
-        }
+        }).dblclick(function () {
+            
+            $(this).addClass("on").siblings("label").removeClass("on");
 
-        $selectors.change(syncLabelStyleToInputValue);
-        syncLabelStyleToInputValue();
+        });
 
-        ///When a legend is clicked, turn on it's checkboxen, unless they were all already on.
-        $("legend").click(function () {
-            var $checkboxen = $(this).siblings("input[type=checkbox]");
-            var numberNotChecked = $checkboxen.not(":checked").length;
-            var turnOn = numberNotChecked > 0;
-            $checkboxen.prop('checked', turnOn);
-            syncLabelStyleToInputValue();
+        ///When a legend is clicked, turn on it's labels, unless they were all already on.
+        $(".boxen legend").click(function () {
+
+            var $boxes: JQuery = $(this).siblings("label");
+            var $offBoxes: JQuery = $boxes.not(".on");
+
+            if ($offBoxes.length == 0) {
+                $boxes.removeClass("on");
+            } else {
+                $offBoxes.addClass("on");
+            }
         });
     });
 })(jQuery);
