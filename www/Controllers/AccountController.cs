c3e12b81ffa50f68d8071bd99db568e22263b79f
buhbuh <JuggleDrum@hotmail.co.uk>
2013-07-28 22:43:59 +0100

    Very simple password authentication

---
+++ www/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using FantasyFootballStats.Models;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid && model.Login())
+            {
+                return RedirectToLocal(returnUrl);
+            }
+
+            // If we got this far, something failed, redisplay form
+            ModelState.AddModelError("", "The password provided is incorrect.");
+            return View(model);
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ www/Models/LoginModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace FantasyFootballStats.Models
+{
+    public class LoginModel
+    {
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Really basic password protection.
+        /// </summary>
+        /// <returns>True if the login was succesful</returns>
+        internal bool Login()
+        {
+            if (IsValidLogin())
+            {
+                FormsAuthentication.SetAuthCookie("registereduser", false);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool IsValidLogin()
+        {
+            string password = ConfigurationManager.AppSettings["password"];
+
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new InvalidOperationException("AppSettings[\"password\"] must be set.");
+            }
+            else
+            {
+                return string.Equals(password, Password, StringComparison.OrdinalIgnoreCase);
+            }
+        }
+    }
+}
