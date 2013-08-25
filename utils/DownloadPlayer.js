e6d87c4b30ba5fdbed7a852477d91a76795d091d
buhbuh <JuggleDrum@hotmail.co.uk>
2013-08-25 17:28:46 +0100

    Refactored download player script to be a node module

--- utils/DownloadPlayer.js
+++ utils/DownloadPlayer.js
@@ -2,11 +2,6 @@
 //This file will download the player data from the fantasy site
 //and save it to your file system in the ./Data/Player2013/ directory.
 
-//To call this command from node, navigate to ./
-//npm install request
-//npm install fs
-//node ./utils/DownloadPlayer 13
-
 function renameProperty(object, oldName, newName) {
     var temp = object[oldName];
     delete object[oldName];
@@ -20,49 +15,50 @@ function scaleCost(object, property) {
 var request = require('request');
 var fs = require('fs');
 
-var playerId = process.argv[2];
+exports.download = function(playerId) {
 
-var url = 'http://fantasy.premierleague.com/web/api/elements/' + playerId + '/';
-var file = './Data/Player2013/' + playerId + '.json';
+    var url = 'http://fantasy.premierleague.com/web/api/elements/' + playerId + '/';
+    var file = './Data/Player2013/' + playerId + '.json';
 
-var json = "";
-request(url).on("data", function(chunk) {
-    chunk = chunk.toString();
-    chunk = chunk.replace(/_/g, "");
-    json += chunk;
-}).on("end", function() {
+    var json = "";
+    request(url).on("data", function(chunk) {
+        chunk = chunk.toString();
+        chunk = chunk.replace(/_/g, "");
+        json += chunk;
+    }).on("end", function() {
 
-    json = JSON.parse(json);
-    
-    //Delete attributes which are not relevant to statistics:
-    delete json.photomobileurl;
-    delete json.shirtimageurl;
-    delete json.shirtmobileimageurl;
-    delete json.newsupdated;
-    delete json.newsadded;
-    delete json.newsreturn;
-    delete json.elementtypeid;
-    delete json.teamcode;
-    delete json.fixtures;
-    
-    renameProperty(json, "webname", "name");
-    renameProperty(json, "nowcost", "cost");
-    
-    
-    //Move the larger properies to the bottom of the output:
-    renameProperty(json, "seasonhistory", "seasonhistory");
-    
-    scaleCost(json, "eventcost")
-    scaleCost(json, "maxcost")
-    scaleCost(json, "mincost")
-    scaleCost(json, "cost")
-    scaleCost(json, "originalcost")
-    
-    json = JSON.stringify(json, undefined, 2);
-    json = json.replace('"seasonhistory"', '\r\n\r\n"seasonhistory"');
-    json = json.replace('"fixtures"', '\r\n\r\n"fixtures"');
-    
-    //console.log(json);
-    
-    fs.writeFile(file, json);
-});
+        json = JSON.parse(json);
+        
+        //Delete attributes which are not relevant to statistics:
+        delete json.photomobileurl;
+        delete json.shirtimageurl;
+        delete json.shirtmobileimageurl;
+        delete json.newsupdated;
+        delete json.newsadded;
+        delete json.newsreturn;
+        delete json.elementtypeid;
+        delete json.teamcode;
+        delete json.fixtures;
+        
+        renameProperty(json, "webname", "name");
+        renameProperty(json, "nowcost", "cost");
+        
+        
+        //Move the larger properies to the bottom of the output:
+        renameProperty(json, "seasonhistory", "seasonhistory");
+        
+        scaleCost(json, "eventcost")
+        scaleCost(json, "maxcost")
+        scaleCost(json, "mincost")
+        scaleCost(json, "cost")
+        scaleCost(json, "originalcost")
+        
+        json = JSON.stringify(json, undefined, 2);
+        json = json.replace('"seasonhistory"', '\r\n\r\n"seasonhistory"');
+        json = json.replace('"fixtures"', '\r\n\r\n"fixtures"');
+        
+        //console.log(json);
+        
+        fs.writeFile(file, json);
+    });
+};
